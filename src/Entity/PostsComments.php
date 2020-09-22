a7f4af2631a0ca93f45c4e6bc22eca01a5050fa0
Rafiou Sitou <rafiousitou90@gmail.com>
2020-09-22 11:19:19 -0300

    feat(entity): Update PostsComments Entity.
    - Update PostsComments entity adding comment replies field.

--- src/Entity/PostsComments.php
+++ src/Entity/PostsComments.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Entity\Traits\Timestampable;
 use App\Repository\PostsCommentsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -49,16 +51,46 @@ class PostsComments
      */
     private string $state;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=PostsComments::class, inversedBy="replies")
+     * @var PostsComments|null
+     */
+    private ?PostsComments $parentComment = null;
+
+    /**
+     * @ORM\OneToMany(targetEntity=PostsComments::class, mappedBy="parentComment")
+     * @var ArrayCollection|PostsComments[]
+     */
+    private $replies;
+
+    /**
+     * PostsComments constructor.
+     */
+    public function __construct()
+    {
+        $this->replies = new ArrayCollection();
+    }
+
+    /**
+     * @return string|null
+     */
     public function getId(): ?string
     {
         return $this->id;
     }
 
+    /**
+     * @return Posts
+     */
     public function getPost(): Posts
     {
         return $this->post;
     }
 
+    /**
+     * @param Posts $post
+     * @return $this
+     */
     public function setPost(Posts $post): self
     {
         $this->post = $post;
@@ -66,11 +98,18 @@ class PostsComments
         return $this;
     }
 
+    /**
+     * @return Users
+     */
     public function getAuthor(): Users
     {
         return $this->author;
     }
 
+    /**
+     * @param Users $author
+     * @return $this
+     */
     public function setAuthor(Users $author): self
     {
         $this->author = $author;
@@ -78,11 +117,18 @@ class PostsComments
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getContent(): string
     {
         return $this->content;
     }
 
+    /**
+     * @param string $content
+     * @return $this
+     */
     public function setContent(string $content): self
     {
         $this->content = $content;
@@ -90,14 +136,79 @@ class PostsComments
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getState(): string
     {
         return $this->state;
     }
 
+    /**
+     * @param string $state
+     * @return $this
+     */
     public function setState(string $state): self
     {
         $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * @return $this|null
+     */
+    public function getParentComment(): ?self
+    {
+        return $this->parentComment;
+    }
+
+    /**
+     * @param PostsComments|null $parentComment
+     * @return $this
+     */
+    public function setParentComment(?self $parentComment): self
+    {
+        $this->parentComment = $parentComment;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getReplies(): Collection
+    {
+        return $this->replies;
+    }
+
+    /**
+     * @param PostsComments $reply
+     * @return $this
+     */
+    public function addReply(self $reply): self
+    {
+        if (!$this->replies->contains($reply)) {
+            $this->replies[] = $reply;
+            $reply->setParentComment($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param PostsComments $reply
+     * @return $this
+     */
+    public function removeReply(self $reply): self
+    {
+        if ($this->replies->contains($reply)) {
+            $this->replies->removeElement($reply);
+            // set the owning side to null (unless already changed)
+            if ($reply->getParentComment() === $this) {
+                $reply->setParentComment(null);
+            }
+        }
 
         return $this;
     }
