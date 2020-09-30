dfc2a08d472b094913bcfe7cc3a94a83b190dd12
Rafiou Sitou <rafiousitou90@gmail.com>
2020-09-29 22:45:51 -0300

    feat(controller): Add webpack dependence.
    - Add webpack dependence
    - Add webpack.config.js

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
